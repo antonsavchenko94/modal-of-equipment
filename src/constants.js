00acdc8a18dff31d5ee01266e0c5ba06d9e24cd7
Anton Savchenko <savchenko.inc@gmail.com>
2017-11-01 17:56:55 +0200

    [update]+/ move request urls to constants

---
+++ src/constants.js
@@ -0,0 +1,10 @@
+// Request urls
+export const TYPES_URL = 'https://api.myjson.com/bins/7y3fr';
+export const MODELS_URL = 'https://api.myjson.com/bins/8jj1j';
+export const VEHICLES_URL = 'https://api.myjson.com/bins/ga5wn';
+
+// Filter parameters
+export const VEHICLE_TYPE_ID = 'vehicleTypeId';
+export const VEHICLE_MODEL_ID = 'vehicleModelId';
+export const VEHICLE_ID = 'vehicleId';
+
--- src/requestUrls.js
+++
@@ -1,9 +0,0 @@
-const TYPES_URL = 'https://api.myjson.com/bins/7y3fr';
-const MODELS_URL = 'https://api.myjson.com/bins/8jj1j';
-const VEHICLES_URL = 'https://api.myjson.com/bins/ga5wn';
-
-export {
-    TYPES_URL,
-    MODELS_URL,
-    VEHICLES_URL
-}
